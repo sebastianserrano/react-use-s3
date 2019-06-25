b90ccf8733195f3a31fbdd9639393059122f2fe0
sebastianserrano <juancho_sss@hotmail.com>
2019-06-24 23:02:23 -0400

    AWS S3 added on demand script for loading aws javascript sdk with s3 client upon request

---
+++ src/s3/s3.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Script from 'react-load-script';
+import PropTypes from 'prop-types';
+
+function onError(){
+  console.log("There was a problem while loading the aws javascript sdk");
+}
+
+function onLoad(apiVersion){
+  window.s3 = window.AWS.s3({apiVersion: apiVersion})
+}
+
+function s3(props){
+  const { apiVersion } = props;
+
+  return(
+    <Script
+      url="https://sdk.amazonaws.com/js/aws-sdk-2.480.0.min.js"
+      onError={onError}
+      onLoad={onLoad(apiVersion ? apiVersion : '2006-03-01')}
+    /> 
+  )
+}
+
+s3.propTypes = {
+  apiVersion: PropTypes.string
+};
+
+export default s3;
