0297e869a5b54a81fbb95b56c70a74af2c6f9d07
sebastianserrano <juancho_sss@hotmail.com>
2019-06-25 21:52:06 -0400

    APP ENTRY added s3 component to entry react file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import S3 from './s3/s3';
 
 function App() {
   return (
     <div className="App">
+      <S3/>
     </div>
   );
 }
