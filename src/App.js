35ff965d24ce6b3848e1a108bfc3ba99e25bf0d5
sebastianserrano <juancho_sss@hotmail.com>
2019-06-27 23:28:10 -0400

    BASE DEMO APP

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import S3 from './s3/s3';
+import FilePicker from './FilePicker';
+import UploadFileButton from './UploadFileButton';
 
 function App() {
+  const [loaded, load] = useState(false);
+  const [file, setFile] = useState("");
+  const [fileLocation, setFileLocation] = useState("To Be Determined");
+  const [result, setResult] = useState('In Progress');
+
+  S3({ load: load, options: {}});
+
   return (
     <div className="App">
-      <S3/>
+      {loaded ? <UploadFileButton file={file} setFileLocation={setFileLocation} setResult={setResult} /> : <h1>loading</h1>}
+      <FilePicker setFile={setFile} />
+      <h1>{result}</h1>
+      <h1>{fileLocation}</h1>
     </div>
   );
 }
