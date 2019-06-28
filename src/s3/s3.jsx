06723bb064aaa2f32cc4742209f7f609a406ae08
sebastianserrano <juancho_sss@hotmail.com>
2019-06-27 23:29:03 -0400

    S3 LOAD SCRIPT changed from class component to react hook for consistency

--- src/s3/s3.jsx
+++ src/s3/s3.jsx
@@ -1,29 +1,26 @@
-import React from 'react';
-import Script from 'react-load-script';
-import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 
-function onError(){
-  console.log("There was a problem while loading the aws javascript sdk");
+function useS3Script(props) {
+  const { load , options } = props;
+  const credentials = {
+    accessKeyId: "",
+    secretAccessKey: ""
+  }
+
+  useEffect(() => {
+    var script = document.createElement('script');
+    script.src = "https://sdk.amazonaws.com/js/aws-sdk-2.480.0.min.js";
+    script.async = true;
+    script.addEventListener('load', function () {
+      window.s3 = new window.AWS.S3(credentials)
+      load(true);
+    });
+    script.addEventListener('error', function (error) {
+      load(false)
+    });
+    document.body.appendChild(script);
+
+  }, [load, credentials, options])
 }
 
-function onLoad(apiVersion){
-  window.s3 = new window.AWS.S3({apiVersion: apiVersion})
-}
-
-function s3(props){
-  const { apiVersion } = props;
-
-  return(
-    <Script
-      url="https://sdk.amazonaws.com/js/aws-sdk-2.480.0.min.js"
-      onError={() => onError()}
-      onLoad={() => onLoad(apiVersion ? apiVersion : '2006-03-01')}
-    /> 
-  )
-}
-
-s3.propTypes = {
-  apiVersion: PropTypes.string
-};
-
-export default s3;
+export default useS3Script;
