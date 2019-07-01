7fe298de71111a14327f82ff864e32256902618a
sebastianserrano <juancho_sss@hotmail.com>
2019-07-01 12:08:03 -0400

    USE AWS UPLOAD WITH FILE HOOK changed syntax on returned callback

--- src/s3/upload/useAWSUploadWithFile.js
+++ src/s3/upload/useAWSUploadWithFile.js
@@ -4,14 +4,16 @@ function useAWSUploadWithFile(props){
   const { file, setFileLocation, setResult } = props;
   const params = {Bucket: 'bucket-name', Key: 'file', Body: file};
 
-  return useCallback(() => {
+  const callback = useCallback(() => {
     window.s3.upload(params).on('httpUploadProgress', (event) => {
       let progress = parseInt((event.loaded * 100) / event.total);
       setResult(progress)
-    }).send((error, data) => {
+    }).send(function(error, data) {
       setFileLocation(data.Location)
     })
-  }, [setFileLocation, setResult, params])
+  }, [setResult, setFileLocation, params])
+
+  return callback
 }
 
 export default useAWSUploadWithFile;
