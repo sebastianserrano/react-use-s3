656b7513c6950f16c41f07c07c4235730ca20403
sebastianserrano <juancho_sss@hotmail.com>
2019-07-02 21:41:44 -0400

    UPLOAD FILE BUTTON component for uploading file to s3 bucket

--- src/UploadFileButton.jsx
+++ src/UploadFileButton.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import useAWSUploadWithFile from './s3/upload/useAWSUploadWithFile';
 
-function UploadFileButton(props){
-  const { file, setFileLocation, setResult } = props;
+function UploadFileButton(props) {
+  const {
+    progress,
+    scriptStatus,
+    file,
+    fileName,
+    setFileLocation,
+    setProgress,
+  } = props;
 
-  const handleClick = useAWSUploadWithFile({ file: file, setFileLocation: setFileLocation, setResult: setResult });
-  return(
-    <button onClick={() => handleClick()}>
-      Upload File
-    </button>
-  )
+  const handleClick = useAWSUploadWithFile({
+    bucket: 'bucket',
+    file,
+    fileName,
+    setFileLocation,
+    setProgress,
+  });
+
+  return (
+    <div className="container-fluid">
+      <div className="row justify-content-center">
+        <button disabled={!scriptStatus || false} type="button" className="btn btn-primary btn-block" id="upload-button" onClick={() => handleClick()}>
+          {progress === 'In Progress' ? 'Upload File' : progress}
+        </button>
+      </div>
+    </div>
+  );
 }
+
+UploadFileButton.propTypes = {
+  progress: PropTypes.string.isRequired,
+  scriptStatus: PropTypes.bool.isRequired,
+  file: PropTypes.string.isRequired,
+  fileName: PropTypes.string.isRequired,
+  setFileLocation: PropTypes.func.isRequired,
+  setProgress: PropTypes.func.isRequired,
+};
 
 export default UploadFileButton;
