3129c1f388d0a7f6eb74dea4fc854a0fe312f48e
sebastianserrano <juancho_sss@hotmail.com>
2019-07-10 21:37:51 -0400

    ENTRY POINT NPM PACKAGE

---
+++ index.js
@@ -0,0 +1,7 @@
+const useAWSScript = require('./dist/script/useAWSScript.js');
+const useAWSUploadWithFile = require('./dist/upload/useAWSUploadWithFile.js');
+
+module.exports = {
+  useAWSScript,
+  useAWSUploadWithFile,
+};
